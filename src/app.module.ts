268c17797940fdd6e57296231dc0917bba2d9a59
rodrigo <javierluis15975@hotmail.com>
2025-01-28 03:08:48 -0500

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { ClientsModule } from './module/clients/clients.module'
 import { PrismaModule } from './prisma/prisma.module'
 import { NotificationEmailModule } from './module/notification-email/notification-email.module'
+import { OrdersModule } from './module/orders/orders.module'
 
 @Module({
-  imports: [ClientsModule, PrismaModule, NotificationEmailModule],
+  imports: [ClientsModule, PrismaModule, NotificationEmailModule, OrdersModule],
   controllers: [],
   providers: [],
 })
