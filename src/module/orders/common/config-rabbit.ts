59a022ab4b52c3c14c06e1f9b8502465ab05d66b
rodrigo <javierluis15975@hotmail.com>
2025-01-31 15:16:38 -0500

    update config-rabbit.ts

--- src/module/orders/common/config-rabbit.ts
+++ src/module/orders/common/config-rabbit.ts
@@ -11,8 +11,23 @@ export const configRabbit = {
   ROUTING_EXCHANGE_SEND_DATA_ORDERS: 'client.send.data.orders',
   ROUTING_ROUTINGKEY_SEND_DATA_ORDERS: 'client.send.data.orders',
   ROUTING_QUEUE_SEND_DATA_ORDERS: 'client.send.data.orders',
+
+  ROUTING_EXCHANGE_GET_ALL_ORDERS: 'client.get.all.order',
+  ROUTING_ROUTINGKEY_GET_ALL_ORDERS: 'client.get.all.order',
+  ROUTING_QUEUE_GET_ALL_ORDERS: 'client.get.all.order',
 }
 export const configQueue: RabbitMQQueueConfig[] = [
+  {
+    name: configRabbit.ROUTING_QUEUE_GET_ALL_ORDERS,
+    routingKey: configRabbit.ROUTING_ROUTINGKEY_GET_ALL_ORDERS,
+    exchange: configRabbit.ROUTING_EXCHANGE_GET_ALL_ORDERS,
+    options: {
+      durable: true,
+      // expires: 60000,
+      // messageTtl: 30000, // Los mensajes expiran si no son consumidos en el tiempo definido.
+      // autoDelete: true, //Borra la cola cuando ya no hay consumidores activos.
+    },
+  },
   {
     name: configRabbit.ROUTING_QUEUE_CREATE_ORDERS,
     routingKey: configRabbit.ROUTING_ROUTINGKEY_CREATE_ORDERS,
@@ -31,6 +46,16 @@ export const configQueue: RabbitMQQueueConfig[] = [
   },
 ]
 export const configExchange: RabbitMQExchangeConfig[] = [
+  {
+    name: configRabbit.ROUTING_EXCHANGE_GET_ALL_ORDERS,
+    type: 'direct',
+    options: {
+      durable: true,
+      // expires: 60000,
+      // messageTtl: 30000, // Los mensajes expiran si no son consumidos en el tiempo definido.
+      // autoDelete: true, //Borra la cola cuando ya no hay consumidores activos.
+    },
+  },
   {
     name: configRabbit.ROUTING_EXCHANGE_CREATE_ORDERS,
     type: 'direct',
